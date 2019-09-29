06f3420a88df6e08c6339e9eb78c1b988729d321
Nathan Woodger <woodgern@gmail.com>
2019-09-29 13:47:38 -0400

    Factor out `Post_metric` function

--- main.go
+++ main.go
@@ -1,51 +1,58 @@
 package main
 
 import (
-  "net/http"
-  "time"
-  "gopkg.in/mgo.v2/bson"
+  "context"
+  "encoding/json"
   "log"
+  "net/http"
 
+  "gopkg.in/mgo.v2/bson"
   "github.com/gin-gonic/gin"
   mongoclient "github.com/woodgern/metric-boy/mongo"
 )
 
-type Metric struct {
-  Event string `json:"event" binding:"required"`
-  Body  string `json:"body" binding:"required"`
-}
-
-
 func main() {
   r := gin.New()
-  if err := mongoclient.Client.Ping(ctx, nil); err != nil {
+  if err := mongoclient.Client.Ping(context.TODO(), nil); err != nil {
     log.Println(err.Error())
   }
 
   collection := mongoclient.Client.Database("metrics").Collection("metrics")
 
   r.GET("/ping", Get_ping)
-  r.POST("/test", func (c *gin.Context) {
-    var requestbody Metric
-    if err := c.ShouldBindJSON(&requestbody); err != nil {
-      c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-      return
-    }
-
-    var bdoc interface{}
-    if err := bson.UnmarshalJSON([]byte(requestbody.Body), &bdoc); err != nil {
-      c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-      return
-    }
-
-    log.Println(&bdoc)
-    collection.InsertOne(ctx, &bdoc)
-
-    c.JSON(http.StatusOK, requestbody)
-  })
+  r.POST("/metric", Post_metric)
   r.Run(":8080")
 }
 
 func Get_ping(c *gin.Context) {
   c.String(http.StatusOK, "pong")
+}
+
+func Post_metric(c *gin.Context) {
+  var requestbody interface{}
+  if err := c.ShouldBindJSON(&requestbody); err != nil {
+    c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+    return
+  }
+
+  jsonString, err := json.Marshal(requestbody)
+  if err != nil {
+    c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+    return
+  }
+
+  var doc interface{}
+  if err := bson.UnmarshalJSON([]byte(jsonString), &doc); err != nil {
+    c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+    return
+  }
+
+  collection.InsertOne(context.TODO(), &doc)
+
+  c.JSON(http.StatusOK, requestbody)
+}
+
+func Get_metrics(c *gin.Context) {
+  start := c.Query("start")
+  end := c.Query("end")
 }
