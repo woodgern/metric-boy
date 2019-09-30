50e458c7b583c00e00c0727a1f487acd523af566
Anthony Robertson <arobertson1796@gmail.com>
2019-09-30 03:13:35 -0400

    fixing main.go and formatting it based on go style guidelines

--- main.go
+++ main.go
@@ -1,58 +1,63 @@
 package main
 
 import (
-  "context"
-  "encoding/json"
-  "log"
-  "net/http"
+	"context"
+	"encoding/json"
+	"log"
+	"net/http"
 
-  "gopkg.in/mgo.v2/bson"
-  "github.com/gin-gonic/gin"
-  mongoclient "github.com/woodgern/metric-boy/mongo"
+	"go.mongodb.org/mongo-driver/mongo"
+
+	"github.com/gin-gonic/gin"
+	mongoclient "github.com/woodgern/metric-boy/mongo"
+	"gopkg.in/mgo.v2/bson"
 )
 
 func main() {
-  r := gin.New()
-  if err := mongoclient.Client.Ping(context.TODO(), nil); err != nil {
-    log.Println(err.Error())
-  }
+	r := gin.New()
+	if err := mongoclient.Client.Ping(context.TODO(), nil); err != nil {
+		log.Println(err.Error())
+	}
 
-  collection := mongoclient.Client.Database("metrics").Collection("metrics")
-
-  r.GET("/ping", Get_ping)
-  r.POST("/metric", Post_metric)
-  r.Run(":8080")
+	r.GET("/ping", GetPing)
+	r.POST("/metric", PostMetric)
+	r.POST("/query", GetMetrics)
+	r.Run(":8080")
 }
 
-func Get_ping(c *gin.Context) {
-  c.String(http.StatusOK, "pong")
+func GetPing(c *gin.Context) {
+	c.String(http.StatusOK, "pong")
 }
 
-func Post_metric(c *gin.Context) {
-  var requestbody interface{}
-  if err := c.ShouldBindJSON(&requestbody); err != nil {
-    c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-    return
-  }
+func PostMetric(c *gin.Context) {
+	var requestbody interface{}
+	if err := c.ShouldBindJSON(&requestbody); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
 
-  jsonString, err := json.Marshal(requestbody)
-  if err != nil {
-    c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-    return
-  }
+	jsonString, err := json.Marshal(requestbody)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
 
-  var doc interface{}
-  if err := bson.UnmarshalJSON([]byte(jsonString), &doc); err != nil {
-    c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-    return
-  }
+	var doc interface{}
+	if err := bson.UnmarshalJSON([]byte(jsonString), &doc); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
 
-  collection.InsertOne(context.TODO(), &doc)
+	collection := getCollection()
+	collection.InsertOne(context.TODO(), &doc)
 
-  c.JSON(http.StatusOK, requestbody)
+	c.JSON(http.StatusOK, requestbody)
 }
 
-func Get_metrics(c *gin.Context) {
-  start := c.Query("start")
-  end := c.Query("end")
+func GetMetrics(c *gin.Context) {
+	return
+}
+
+func getCollection() *mongo.Collection {
+	return mongoclient.Client.Database("metrics").Collection("metrics")
 }
